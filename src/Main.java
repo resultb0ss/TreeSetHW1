2b1cb4aa5f5ac78265990454a4230da25100c1d9
Admin <reteamboss@gmail.com>
2024-09-24 14:19:11 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<Integer,String> myMap = new HashMap<>();
+        myMap.put(1,"John");
+        myMap.put(2,"Tom");
+        myMap.put(3,"Helen");
+        myMap.put(4,"Jacob");
+        myMap.put(5,"Clara");
+
+        System.out.println(getKey(myMap));
+    }
+
+    public static Set <Integer> getKey(HashMap<Integer,String> hashMap){
+        return hashMap.keySet();
     }
 }
+
